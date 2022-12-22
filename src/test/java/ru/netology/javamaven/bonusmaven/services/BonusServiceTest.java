c6115c48805f94c034e3b38a68f70b6bb4ebc217
Luyba <lsorlova82@gmail.com>
2022-12-22 21:05:07 +0200

    added parametrized test

---
+++ src/test/java/ru/netology/javamaven/bonusmaven/services/BonusServiceTest.java
@@ -0,0 +1,15 @@
+package ru.netology.javamaven.bonusmaven.services;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv")
+    void shouldCalculateForRegisteredAndUnderLimit(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+}
